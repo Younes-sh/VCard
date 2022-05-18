24705800ea1ce748c6aa4e4d735ce75e49f5d08c
younes sheikhlar <sheikhlaryounes@mail.com>
2022-05-18 23:00:18 +0200

    branch About : created Menu Burger

--- JS/index.js
+++ JS/index.js
@@ -11,19 +11,35 @@ function change() {
     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
   }
 }
+
+
 //      Home 
-const home = document.querySelector('#home');
+const home = document.querySelector('.home');
 home.addEventListener('click',homeHandler)
 function homeHandler(){
-  about.classList.toggle('open')
+  about.classList.add(' display')
 }
 
 //      About 
 
 const aboutBtn = document.querySelector('#about');
 const about = document.querySelector('.about');
+const bottemColor = document.querySelector('.bottem-about')
+
 aboutBtn.addEventListener('click' , aboutHandler)
 function aboutHandler(){
-  about.classList.toggle('open')
-
+  about.classList.toggle('display')
+  bottemColor.classList.toggle('border-color-about')
 }
+
+
+//     Skills
+//      Css
+
+//      Html
+
+//      Javascript
+
+//      Git
+
+//      Bootstrap
